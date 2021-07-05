622e5a4f3fd91876f1ec37624140116fcf433cad
Ka Wing Tsang <kawingt2@gmail.com>
2021-07-05 15:54:20 +0800

    bproject set up

---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Header from './Header'
+
+export default function Layout({title, keywords, description, children}) {
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta name="keywords" content={keywords} />
+                <meta name="description" content={description} />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header/>
+            <main className='container mx-auto my-7'>
+                {children}
+            </main>
+        </div>
+    )
+}
+
+Layout.defaultProps = {
+    title: 'Welcome to DevSpace',
+    keywords: "Web development, programing, coding",
+    description: 'A guide for aspiring web developer'
+}
