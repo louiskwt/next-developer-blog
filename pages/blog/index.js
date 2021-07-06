e072f4f0f738819da0355ffded5458a4c92fb3b8
Ka Wing Tsang <kawingt2@gmail.com>
2021-07-06 16:22:45 +0800

    displaying and sorting posts and post category

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,41 @@
-import Layout from "../../components/Layout"
+import Layout from '../../components/Layout';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import Post from '../../components/Post';
+import { sortByDate } from '../../utils/index';
 
-export default function BlogPage() {
-    return (
-        <Layout>
-            <h1>Blog</h1>
-        </Layout>
-    )
+export async function getStaticProps() {
+	const files = fs.readdirSync(path.join('posts'));
+	// creating slug
+	const posts = files.map((filename) => {
+		const slug = filename.replace('.md', '');
+		const markdownWithMeta = fs.readFileSync(
+			path.join('posts', filename),
+			'utf-8'
+		);
+
+		const { data: frontmatter } = matter(markdownWithMeta);
+
+		return { slug, frontmatter };
+	});
+	console.log(posts);
+	return {
+		props: {
+			posts: posts.sort(sortByDate)
+		}
+	};
+}
+
+export default function BlogPage({ posts }) {
+	return (
+		<Layout>
+			<h1 className='text-5xl border-b-4 p-5 font-bold'>All Blogs</h1>
+			<div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+				{posts.map((post, index) => (
+					<Post key={index} post={post} />
+				))}
+			</div>
+		</Layout>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,48 @@
-import Layout from '../components/Layout'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-
-export default function HomePage({ posts }) {
-
-  console.log(posts)
-  return (
-    <Layout>
-      <h1>Hello</h1>
-    </Layout>
-  )
-}
+import Layout from '../components/Layout';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import Link from 'next/link';
+import Post from '../components/Post';
+import { sortByDate } from '../utils/index';
 
 export async function getStaticProps() {
-  const files = fs.readdirSync(path.join('posts'));
-  // creating slug
-  const posts = files.map(filename => {
-    const slug = filename.replace('.md', '');
-    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+	const files = fs.readdirSync(path.join('posts'));
+	// creating slug
+	const posts = files.map((filename) => {
+		const slug = filename.replace('.md', '');
+		const markdownWithMeta = fs.readFileSync(
+			path.join('posts', filename),
+			'utf-8'
+		);
 
-    const {data: frontmatter} = matter(markdownWithMeta);
+		const { data: frontmatter } = matter(markdownWithMeta);
 
-    return {slug, frontmatter}
-    
-  })
-  console.log(posts)
-  return {
-    props: {
-      posts
-    },
-  }
+		return { slug, frontmatter };
+	});
+	console.log(posts);
+	return {
+		props: {
+			posts: posts.sort(sortByDate).slice(0, 6)
+		}
+	};
+}
+
+export default function HomePage({ posts }) {
+	console.log(posts);
+	return (
+		<Layout>
+			<h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Post</h1>
+			<div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+				{posts.map((post, index) => (
+					<Post key={index} post={post} />
+				))}
+			</div>
+			<Link href='/blog'>
+				<a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
+					All Posts
+				</a>
+			</Link>
+		</Layout>
+	);
 }
